5918d42f0e58cf81f4a67887065e1cd6dc5281e9
HyunM <wisek0226@gmail.com>
2020-06-26 16:17:25 -0700

    map function with key

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css'
 function NumberList(props) {
     const numbers = props.numbers;
     const listItems = numbers.map((number) =>
-        <li>
+        <li key={number.toString()}>
             {number}
         </li>
     );
