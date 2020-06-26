584e5f6596c3fe569a15b85aadf8fc727ee23ec5
HyunM <wisek0226@gmail.com>
2020-06-26 16:28:22 -0700

    Correct Key Usage

--- src/index.js
+++ src/index.js
@@ -2,15 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
+function ListItem(props) {
+    const value = props.value;
+    return <li>{props.value}</li>
+}
+
 function NumberList(props) {
     const numbers = props.numbers;
     const listItems = numbers.map((number) =>
-        <li key={number.toString()}>
-            {number}
-        </li>
+        <ListItem key={number.toString()} value={number} />
     );
     return (
-        <ul>{listItems}</ul>
+        <ul>
+            {listItems}
+        </ul>
     );
 }
 
