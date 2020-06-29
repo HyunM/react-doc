57d02d1b589d5cacde8fe56678e59518cef3ff36
HyunM <wisek0226@gmail.com>
2020-06-29 10:05:41 -0700

    Specialization 1

--- src/index.js
+++ src/index.js
@@ -2,37 +2,33 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
-function Contacts() {
-    return <div className="Contacts" />;
-}
-
-function Chat() {
-    return <div className="Chat" />;
-}
-
-
-function SplitPane(props) {
-    return (
-        <div className="SplitPane">
-            <div className="SplitPane-left">
-                {props.left}
-            </div>
-            <div className="SplitPane-right">
-                {props.right}
-            </div>
+function FancyBorder(props) {
+    return(
+        <div className={'FancyBorder FancyBorder-' + props.color}>
+            {props.children}
         </div>
+    );
+}
+
+function Dialog(props) {
+    return(
+        <FancyBorder color="blue">
+            <h1 className="Dialog-title">
+                {props.title}
+            </h1>
+            <p className="Dialog-message">
+                {props.message}
+            </p>
+        </FancyBorder>
     );
 }
 
 function WelcomeDialog() {
     return (
-        <SplitPane
-            left={
-                <Contacts />
-            }
-            right={
-                <Chat />
-            } />
+        <Dialog
+            title="Welcome"
+            message="Thank you for visiting our spacecraft"
+        />
     );
 }
 
